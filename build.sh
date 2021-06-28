b75d9f10525853f38e454cb8e3e018c647b744f4
Michael Zinn <michaelgzinn@gmail.com>
2021-06-28 22:57:34 +0200

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# clear build directory
+
+## Delete the old build directory if it exists
+if [[ -d build ]]; then
+  rm -rf build
+fi
+## create a new one
+mkdir build
+
+
+
+# Build 7K_Settlerclonk.c4d
+
+## Copy, excluding files that weren't there originally
+rsync -a \
+  --exclude=.git \
+  --exclude=.gitignore \
+  --exclude=build \
+  --exclude=build.sh \
+  --exclude=LICENSE \
+  ./ build/7K_Settlerclonk.c4d
+
+## Pack
+c4group build/7K_Settlerclonk.c4d -p
